4c76a81df40efc6306fc0f89e9ffd9385c6a30a0
Ben Fridolin <benfridolin@icloud.com>
2021-09-26 18:40:13 +0200

    Added ECR sdk repo

---
+++ ECR/create_repository.py
@@ -0,0 +1,33 @@
+# Use this template to create a repository in ECR
+
+# Reference link
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecr.html#ECR.Client.create_repository
+
+import boto3
+import os
+
+os.environ['AWS_PROFILE'] = "....."
+os.environ['AWS_DEFAULT_REGION'] = "....."
+
+
+# Set ECR as client
+client = boto3.client('ecr')
+
+
+# Refer link for more detailed information. The registryId can be ignored as using it results in error(Unknown parameter in input: "registryId").
+response = client.create_repository(
+    repositoryName='string',
+    tags=[
+        {
+            'Key': 'string ',
+            'Value': 'string'
+        },
+    ],
+    imageTagMutability='MUTABLE',
+    imageScanningConfiguration={
+        'scanOnPush': False
+    },
+    encryptionConfiguration={
+        'encryptionType': 'AES256'
+    }
+)
