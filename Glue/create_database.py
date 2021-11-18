e5decaccc18037aa8049418222f15db85b764903
Ben Fridolin <benfridolin@icloud.com>
2021-11-18 12:36:22 +0100

    Added script to create Glue database

---
+++ Glue/create_database.py
@@ -0,0 +1,23 @@
+# Use this template to create a new database in glue
+
+# Reference link
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/glue.html#Glue.Client.create_database
+
+import boto3
+import os
+
+os.environ['AWS_PROFILE'] = "______"
+os.environ['AWS_DEFAULT_REGION'] = "______"
+
+# Set Glue as client
+client = boto3.client('glue')
+
+response = client.create_database(
+    DatabaseInput={
+        'Name': '______',
+        'Description': '______',
+        'LocationUri': '___s3_bucket_location___'
+    }
+)
+
+print(response)
